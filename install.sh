6e67e38b561136303dfd09a34fa5564eb9c177ed
Eric S. Bullington <eric.s.bullington@gmail.com>
2014-10-10 11:00:38 -0400

    Removing commas form apt-get install list and changing stub echo call to ln

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
+# System dependencies
+sudo apt-get install curl git vim-gtk tmux build-essential
 
 # Create environment file to store local user envvars
 [ ! -f $HOME/.environment ] && touch $HOME/.environment
 
 # Symlink all dotfiles to home directory
-IFS=$'\n'; for f in $(find $PWD -name ".*" -type f); do echo $f $HOME/$(basename $f); done;
-
-# System dependencies
-sudo apt-get install curl, git, vim-gtk, tmux, build-essential
+IFS=$'\n'; for f in $(find $PWD -name ".*" -type f); do ln -s $f $HOME/$(basename $f); done;
 
 # Install vim tools
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
