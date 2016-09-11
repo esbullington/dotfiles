22d726f295fdda6c3abb41fe1a7858b0b2d75e9b
Eric S. Bullington <eric.s.bullington@gmail.com>
2016-09-11 07:15:40 -0400

    Add guard to prevent deletion of existing environmental vars

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,15 +5,17 @@ DOTFILES_DIR=$HOME/repos/dotfiles
 # System dependencies
 sudo apt-get install -y curl git vim-gtk tmux build-essential stow
 
-# Create environment file to store local user envvars
-[ ! -f $HOME/.environment ] && touch $HOME/.environment
-
 # Symlink all dotfiles to home directory
 cd $DOTFILES_DIR
 stow bash --target=$HOME --dir=$DOTFILES_DIR
 stow git --target=$HOME --dir=$DOTFILES_DIR
 stow tmux --target=$HOME --dir=$DOTFILES_DIR
 stow vim --target=$HOME --dir=$DOTFILES_DIR
+
+# Create environment file to store local user envvars
+if [ ! -e $HOME/.bash/.environment ]; then
+    touch $HOME/.bash/.environment
+fi
 
 # Install vim tools
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
